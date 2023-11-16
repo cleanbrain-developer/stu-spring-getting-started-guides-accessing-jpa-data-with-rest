fd2aad2caaae4f19da51269cc125a9864eb0c7ee
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-16 20:59:04 +0900

    first commit

---
+++ src/main/java/com/example/StuSpringGettingStartedGuidesAccessingJpaDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringGettingStartedGuidesAccessingJpaDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringGettingStartedGuidesAccessingJpaDataWithRestApplication.class, args);
+	}
+
+}
